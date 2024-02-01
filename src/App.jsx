976f98687e554800b67c28808a4753b0b0497cb0
jtovar <jtovar@ks2.tech>
2024-02-01 18:07:37 -0400

    Agregado nuevos componenetes, la librería swipe y hecho los calculos

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import Modal from './components/Modal'
+import ListadoGastos from './components/ListadoGastos'
+import { generarId } from './helpers'
 import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 
@@ -7,22 +10,64 @@ function App() {
 
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [animarModal, setAnimarModal] = useState(false);
+  const [gastos, setGastos] = useState([]);
+
+  const handleNuevoGasto = () =>{
+    setModal(true);
+
+    setTimeout(() => {
+      setAnimarModal(true);
+    }, 500);
+  }
+
+  const guardarGastos = gasto => {
+    gasto.id = generarId();
+    gasto.fecha = Date.now();
+    setGastos([...gastos, gasto]);
+
+    setAnimarModal(false);
+
+    setTimeout(() => {
+        setModal(false);
+    }, 500);
+  }
 
   return (
-    <div>
+    <div className={modal ? 'fijar' : ''}>
       <Header 
         presupuesto={presupuesto}
         setPresupuesto={setPresupuesto}
         isValidPresupuesto={isValidPresupuesto}
         setIsValidPresupuesto={setIsValidPresupuesto}
+        gastos={gastos}
       />
 
     {isValidPresupuesto ? (
+      <>
+      <main>
+        <ListadoGastos
+          gastos={gastos}
+        />
+      </main>
       <div className='nuevo-gasto'>
-          <img src={IconoNuevoGasto} alt="imágen nuevo gasto" />
+          <img 
+           src={IconoNuevoGasto} 
+           alt="imágen nuevo gasto" 
+           onClick={handleNuevoGasto}
+           />
       </div>
+      </>
     ) : null}
       
+      {modal && <Modal 
+                  setModal={setModal}
+                  animarModal={animarModal}
+                  setAnimarModal={setAnimarModal}
+                  guardarGastos={guardarGastos}
+                />
+      }
 
     </div>
   )
